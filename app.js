d97640775612bced3b73e95a560545fe019d5756
arjun11120 <arjunarjun2159@gmail.com>
2022-09-16 16:59:10 +0530

    row and column added

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let dice = document.getElementById('dice');
 var outputDiv = document.getElementById('diceResult');
-
-
 function rollDice() {
-    let result = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-    dice.dataset.side = result;
-    dice.classList.toggle("reRoll");
-    console.log(result);
-	alert(result);
-	var re = (result*10)+4;
-	var left = re+'%'; 
+   var x = document.getElementById("dotg");
+   let result = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+   dice.dataset.side = result;
+   dice.classList.toggle("reRoll");
+   //console.log(result);
+	//alert(result);
+	//var re = (result*10)+4;
+	//var left = re+'%'; 
+    //$("#r-dot").animate({left:left});
+    $("#r-dot").prependTo($('#1'));
 
-    $("#r-dot").animate({left:left});
 }
 dice.addEventListener("click", rollDice);
 //var left = (4*10)+4;
